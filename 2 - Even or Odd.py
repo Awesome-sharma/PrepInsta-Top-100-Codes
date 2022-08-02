5970b7d28ce26acd7115845b23bb8f79126a090c
Ayush Sharma <81407894+Awesome-sharma@users.noreply.github.com>
2022-08-02 14:02:50 +0530

    Create 2 - Even or Odd.py

---
+++ 2 - Even or Odd.py
@@ -0,0 +1,11 @@
+# Check Whether a Number is Even or Odd
+# Given an integer input num, the objective is to write a code to Check Whether a Number is Even or Odd in Python. To do so we check if the number is divisible by 2 or not, it’s Even if it’s divisible otherwise Odd.
+
+# Example 
+# Input : num = 3
+# Output : Odd 
+
+
+num = int(input("give value = "))
+
+print("even" if num%2==0 else "odd")
