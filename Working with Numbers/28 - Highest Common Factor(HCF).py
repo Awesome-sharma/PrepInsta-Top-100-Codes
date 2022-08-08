10de05882ed488295c948c8ea2e962f1a7020f27
Ayush Sharma <81407894+Awesome-sharma@users.noreply.github.com>
2022-08-08 22:21:29 +0530

    Create 28 - Highest Common Factor(HCF).py

---
+++ Working with Numbers/28 - Highest Common Factor(HCF).py
@@ -0,0 +1,7 @@
+# HCF of Two Numbers also known as GCD (Greatest Common Divisor).
+
+# x is called HCF of a & b two conditions :
+
+# x can completely divide both a & b leaving remainder 0
+# No, other number greater than x can completely divide both a & b
+
